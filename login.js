9184f220ffe657f5c2873134d8136f3c867f7529
Nestor Melendez <melendezinv4321@gmail.com>
2024-06-11 16:13:24 -0400

    LOGIN COMPLET

--- login.js
+++ login.js
@@ -32,6 +32,7 @@ let comments = [];
 let CountedComment = 0;
 
 let userActive = {};
+let userStorage = "Users";
 
 let pageLogin = document.getElementById("page-login");
 let pagePost = document.getElementById("generate-posts");
@@ -41,72 +42,114 @@ let password = document.getElementById("password");
 let email = document.getElementById("email");
 let generatePosts = document.getElementById("generate-posts");
 
-document.addEventListener("click", (e) => {
+/* let userStorage = "users"; */
+
+document.addEventListener("click", async (e) => {
   if (e.target.matches(".user-out")) {
     pagePost.classList.toggle("disguise");
     pageLogin.classList.toggle("disguise");
+    userActive = ""
     nav.innerHTML = ""
-    
+
   }
 
   if (e.target.matches(".btn-sign-up")) {
-    if (email.value === "") {
-      alert("el campo usuario es obligatorio");
-      return false;
-    } else if (password.value === "") {
-      alert("el campo contraseña es obligatorio");
-      return false;
-    }
-    userActive = {};
 
-    verifyEmail(user, email.value);
 
-    if (!userActive.email) {
-      alert("Disculpe el usuario no se encuentra registrado");
-      return;
-    }
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-    if (userActive.password == password.value) {
+    const raw = JSON.stringify({
+      email: email.value,
+      password: password.value,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:4000/auth/login",
+        requestOptions
+      );
+      const loginData = await response.json();
+      console.log(
+        "loginData",
+        loginData,
+        "response",
+        response,
+        loginData.user.name.charAt(0)
+      );
+
+      let usersGroup = localStorage.getItem(userStorage);
+      let users = [];
+      if (usersGroup) {
+        users = JSON.parse(usersGroup);
+      }
+      let user = {
+        token: loginData.token,
+        user: {
+          id: loginData.user.id,
+          name: loginData.user.name,
+          email: loginData.user.email,
+          createdAt: loginData.user.createdAt,
+          updatedAt: loginData.user.updatedAt,
+        },
+      };
+
+      users.push(user);
+
+      const saveUser = JSON.stringify(users);
+      localStorage.setItem(loginData.user.name, saveUser);
+
+
+
+      userActive = user
+
       pagePost.classList.toggle("disguise");
       pageLogin.classList.toggle("disguise");
 
       let menu = `<div class="menu-user">
-                    <div class="photo-profile">
-                      <img src="${userActive.photo}" alt="">
-                    </div>
-                  <h2> ${userActive.userName}</h2>
+              <div class="photo-profile-avatar">
+                <span>${loginData.user.name.charAt(0)}</span>
+              </div>
+                  <h2> ${loginData.user.name}</h2>
                     </div>
                   </div>
 
                   <textarea id="input-post" class="input-post" name="" rows="3" maxlength="255" placeholder="¿Que estas pensando?"></textarea>
-
-                  <div class="btn-options"> 
-                    <button id="create-post" class="btn --menu-user create-post">Publicar</button>
-                    <button id="user-out" class="btn --menu-user user-out">Salir</button>
-                  </div>`;
+              </div>                        
+              <div class="btn-options"> 
+                <button id="create-post" class="btn --menu-user create-post">Publicar</button>
+                <button id="user-out" class="btn --menu-user user-out">Salir</button>
+              </div>`;
 
       nav.innerHTML = menu;
-
-
-
-
-      if (!posts.length == 0) {
-        generatePostsHtml();
-      } else {
-        pagePost.innerHTML = "";
-      }
-    } else {
-      alert("disculpe la contraseña no es valida");
+    } catch (error) {
+      console.error(error);
     }
+
+    if (!posts.length == 0) {
+      generatePostsHtml();
+    } else {
+      pagePost.innerHTML = "";
+    }
+  } else {
+    /* alert("disculpe la contraseña no es valida"); */
   }
+  // }
 
   if (e.target.matches(".create-post")) {
     CountedPublications++;
     let inputPost = document.getElementById("input-post").value;
-    let now = getTime()
+    let now = getTime();
     let newPost = {
       id: CountedPublications,
-      userId: userActive.userName,
+      userId: userActive.user.name,
       text: inputPost,
       date: now,
       image: userActive.photo,
@@ -115,10 +158,6 @@ document.addEventListener("click", (e) => {
     generatePostsHtml();
 
     document.getElementById("input-post").value = "";
-    
-
-
-    
   }
 
   if (e.target.matches(".btn-like")) {
@@ -127,13 +166,13 @@ document.addEventListener("click", (e) => {
     let post = posts[indice];
     let newLike = {
       id: CountedLikes,
-      userId: userActive.userName,
+      userId: userActive.name,
       postId: post.id,
     };
 
     likes.push(newLike);
     let cuantosLikes = foundLikes(likes, post.id);
-    
+
     generatePostsHtml();
   }
 
@@ -148,8 +187,8 @@ document.addEventListener("click", (e) => {
     let post = posts[indice];
 
     let newComment = {
-      id: CountedLikes,
-      userId: userActive.userName,
+      id: CountedComment,
+      userId: userActive.user.name,
       postId: post.id,
       text: inputCommentValue,
       image: userActive.photo,
@@ -165,13 +204,13 @@ document.addEventListener("click", (e) => {
 function getTime() {
   let date = new Date();
   let year = date.getFullYear();
-  let month = date.getMonth() + 1; 
-  let day = date.getDate(); 
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
   let hh = date.getHours();
   let mm = date.getMinutes();
   let ss = date.getSeconds();
   let now = `${day}/${month}/${year} - ${hh}:${mm}:${ss}`;
-  return now; 
+  return now;
 }
 
 function verifyEmail(user, email) {
@@ -214,10 +253,10 @@ function makeComment(comments, postId) {
     const element = comentario[index];
     makeComment += `
          <div class="post-header-user">
-         <div class="photo-profile">
-           <img src=${element.image} alt="">
-         </div>
-         <div class="data-user-post">
+          <div class="photo-profile-avatar-comment">
+            <span>${element.userId.charAt(0)}</span>
+          </div>   
+          <div class="data-user-post">
            <h2> ${element.userId}</h2>
            <h2> ${element.date}</h2>
          </div>
@@ -225,7 +264,6 @@ function makeComment(comments, postId) {
            <span>
            ${element.text}
            </span>
- 
          </div>
        </div>         
          `;
@@ -242,53 +280,56 @@ const generatePostsHtml = () => {
     const makeComments = makeComment(comments, element.id);
 
     poster += ` 
-    <article class="content post">
-    <header class="post-header">
-      <div class="post-header-user">
-        <div class="photo-profile">
-          <img src="${element.image}" alt="">
-        </div>
-        <div class="data-user-post">
-          <h2> ${element.userId}</h2>
-          <h2> ${element.date}</h2>
-        </div>
-      </div>
-      <div class="btn-options">
-        <button class="btn --option">...</button>
-      </div>
-    </header>
+           <article class="content post">
+               <header class="post-header">
+                     <div class="post-header-user">
+                              <div class="photo-profile-avatar">
+                                     <span>${element.userId.charAt(0)}</span>
+                                     
+   </div>
+           <div class="data-user-post">
+                     <h2> ${element.userId}</h2>
+                               <h2> ${element.date}</h2>
+                                       </div>
 
-    <div class="container  post-content">
-      <span>
-        ${element.text}
-      </span>
-      <div id="post-likes" class="post-likes">
-        <h5>❤️ ${postLikes}</h5>
-        <h5>${postComments} Comentarios</h5>
-      </div>
-    </div>
 
-    <footer class="btns-comment">
-      <button data-like="${index}" class="btn --btn-post btn-like">Me gusta</button>
-      <button data-comment="${index}" class="btn --btn-post btn-comment">Comentar</button>
-      <button class="btn --btn-post">Compartir</button>
-    </footer>
-
-    <article class="container-comment">
-      ${makeComments}
-     
+                                       <div class="btn-options">
+                                               <button class="btn --option">...</button>
+                                                     </div>
+ </div>
+                         </header>
+                         
+                             <div class="container  post-content">
+                                   <span>
+                                           ${element.text}
+     </span>
+           <div id="post-likes" class="post-likes">
+                   <h5>❤️ ${postLikes}</h5>
+                           <h5>${postComments} Comentarios</h5>
+                                 </div>
+                                     </div>
+                                     
+                                         <footer class="btns-comment">
+   <button data-like="${index}" class="btn --btn-post btn-like">Me gusta</button>
+         <button data-comment="${index}" class="btn --btn-post btn-comment">Comentar</button>
+               <button class="btn --btn-post">Compartir</button>
+                   </footer>
+                   
+                       <article class="container-comment">
+                             ${makeComments}
+                                  
       <div class="post-contentss">
         <div class="post-header-user">
-          <div class="photo-profile">
-            <img src=${userActive.photo} alt="">
-          </div>
-          <input id="${index}" class="input-comment" type="text" placeholder="     Comentar como ${userActive.userName}">
+          <div class="photo-profile-avatar">
+            <span>${userActive.user.name.charAt(0)}</span>
+          </div>   
+          <input id="${index}" class="input-comment" type="text" placeholder="     Comentar como ${userActive.user.name}">
+          <button data-comment="${index}" class="btn --btn-comment btn-comment">Comentar</button>    
         </div>
-
       </div>
-
-    </article>
-  </article>`;
+                
+                    </article>
+                      </article>`;
   }
 
   pagePost.innerHTML = poster;
